8c63fb31246ba0ed5f6a448977a34511681453f3
Crowdbotics <team@crowdbotics.com>
2023-08-16 02:36:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-cake-42867",
+  url: "https://blue-cake-42867.botics.co",
+  api: "https://blue-cake-42867.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
